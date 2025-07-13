b701649f6df845f8da6c7c8db9d5da5d758c6240
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-07-13 18:51:55 +0200

    rregullim

--- src/main/java/com/example/studentmenagmentapp/Services/StudentService.java
+++ src/main/java/com/example/studentmenagmentapp/Services/StudentService.java
@@ -3,14 +3,21 @@ package com.example.studentmenagmentapp.Services;
 import com.example.studentmenagmentapp.Models.Student;
 import com.example.studentmenagmentapp.Repository.StudentRepository;
 
+import java.util.List;
+
 public class StudentService {
     private StudentRepository studentRepository;
 
     public  StudentService(){
         studentRepository=new StudentRepository();
     }
+
     public void shtoStudent(String emer,String mbiemer,String email){
         Student student=new Student(emer,mbiemer,email);
         studentRepository.ShtoTeDhena(student);
+    }
+
+    public List<Student> gjejTeGjitheStudentet() {
+        return studentRepository.gjejTeGjithe();
     }
 }
