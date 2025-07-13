d9c1aa400288ec45ae2be3ab8d7e50d1bd7e3355
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-07-13 18:52:47 +0200

    thirrja ne main

--- src/main/java/com/example/studentmenagmentapp/App.java
+++ src/main/java/com/example/studentmenagmentapp/App.java
@@ -1,13 +1,32 @@
 package com.example.studentmenagmentapp;
 
-public class App {
-    public static void main(String[] args){
-        DBConnection db=new DBConnection();
-        db.getConnection();
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-        KrijoTable.krijoTable();
-        ShtoTeDhena.shtoStudentin();
+import java.io.IOException;
+import java.net.URL;
 
+public class App extends Application {
+    public void start(Stage primaryStage) {
+        try {
+            URL fxmlUrl = getClass().getResource("/studentfxml.fxml");
 
+            System.out.println("FXML file location: " + fxmlUrl);
+
+            if (fxmlUrl == null) {
+                throw new RuntimeException("FXML file not found!");
+            }
+
+            FXMLLoader fxmlLoader = new FXMLLoader(fxmlUrl);
+            Scene scene = new Scene(fxmlLoader.load());
+            primaryStage.setTitle("Menaxhimi i Studentëve");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
