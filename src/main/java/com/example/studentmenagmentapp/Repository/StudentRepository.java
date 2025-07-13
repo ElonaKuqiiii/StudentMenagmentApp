f2493f7fa8dc34173fb7f64a2d7123a6a8e816b9
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-07-13 18:52:02 +0200

    rregullim

--- src/main/java/com/example/studentmenagmentapp/Repository/StudentRepository.java
+++ src/main/java/com/example/studentmenagmentapp/Repository/StudentRepository.java
@@ -4,9 +4,9 @@ import com.example.studentmenagmentapp.DBConnection;
 import com.example.studentmenagmentapp.Models.Student;
 
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentRepository {
     public void ShtoTeDhena(Student student){
@@ -22,5 +22,30 @@ public class StudentRepository {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public List<Student>gjejTeGjithe(){
+        List<Student>studentet=new ArrayList<>();
+        String query="SELECT * FROM students";
+        try{
+            Connection con=DBConnection.getConnection();
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery(query);
+            while (rs.next()) {
+                Student s = new Student(
+                        rs.getInt("id"),
+                        rs.getString("emer"),
+                        rs.getString("mbiemer"),
+                        rs.getString("email")
+
+                );
+                studentet.add(s);
+            }
+
+        }catch (SQLException e) {
+            System.err.println("Gabim gjatë marrjes së studentëve: " + e.getMessage());
+        }
+        return studentet;
+
     }
 }
