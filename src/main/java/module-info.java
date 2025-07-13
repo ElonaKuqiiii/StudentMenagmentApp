106c954740009d09b5cf216bdd36f6dbf80be7f9
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-07-13 18:52:13 +0200

    rregullim

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,8 @@ module com.example.studentmenagmentapp {
 
     opens com.example.studentmenagmentapp to javafx.fxml;
     exports com.example.studentmenagmentapp;
+    opens com.example.studentmenagmentapp.Controllers to javafx.fxml;
+
+    //exports com.example.studentmenagmentapp;
+    exports com.example.studentmenagmentapp.Controllers;
 }
